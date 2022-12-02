2448183e0bc86ae78593034457c36fb63a55ca32
Elidiane Bezerra <elidiane.asb@gmail.com>
2022-12-02 02:26:47 +0000

    fixed appointments list

--- src/pages/AdminBookings.jsx
+++ src/pages/AdminBookings.jsx
@@ -54,20 +54,21 @@ function AdminBookings(props) {
   return (
     <div>
       <div className="list ms-4 me-4 pt-5 pb-4 mt-5">
-        <h4 className="text-start pt-4">My Appointments</h4>
+        <h4 className="text-start pt-5">My Appointments</h4>
         <hr />
         <div className="d-flex justify-content-between" >
           <h6><b>Service</b></h6>
           <h6><b>Date</b></h6>
           <h6><b>Hour</b></h6>
           <h6><b>Status</b></h6>
-          <h6><b>Client</b></h6>
         </div>
         {bookings && bookings.map(book => (
           <div>
             <p className="fw-lighter text-start">Client: {(book.user.name)}</p>
+          <div>
             <Booking book={book} cancelBook={cancelBook} acceptBook={acceptBook} showAccept={true} />
             <hr />
+          </div>
           </div>
         )
         )}
